28a6a03731e284714ffcfac385ce9e0655b5d2db
Mathis Favier <favier.mathis@gmail.com>
2024-06-03 10:00:27 +0200

    Commented out the actual oop project

--- src/oo_ll.c
+++ src/oo_ll.c
@@ -7,10 +7,17 @@
 #include "lua.h"
 #include "lua_list.h"
 
+/** create a new instance for the lua's linked list.
+ *
+ * @param L The lua_Stack on which it has been called.
+ *
+ * @return the number of arguments returned.
+ */
 static int new_object(lua_State* L) {
     int type = lua_type(L, 1);
     void* udata;
 
+    // handle each acceptable type and throw error if it's not a good one.
     switch (type) {
         case LUA_TSTRING:
             udata = lua_newuserdata(L, sizeof(const char*));
@@ -39,17 +46,23 @@ static int new_object(lua_State* L) {
             break;
     }
 
+    // Creation of the table for set it as a metatable.
     const char* key = "__index";
     lua_newtable(L);
     void* head_node = lua_newuserdata(L, sizeof(ONode_t));
     lua_setfield(L, -2, key);
 
+    // create the metatable on self in orer to create the new lua object.
     lua_pushlightuserdata(L, &head_node);
     lua_setmetatable(L, -1);
 
     return 1;
 }
 
+/** Helper function that prints the actual stack into stdout.
+ *
+ * @param L the lua Stack
+ */
 void print_stack(lua_State* L) {
     int top = lua_gettop(L);  // Get the number of elements on the stack
     printf("Stack size: %d\n", top);
@@ -76,6 +89,9 @@ void print_stack(lua_State* L) {
     }
 }
 
+/**
+ * Lua's lib related
+ */
 static const struct luaL_Reg linkedlist[] = {{"new", new_object}, {NULL, NULL}};
 
 int luaopen_libluall(lua_State* L) {
